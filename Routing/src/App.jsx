0b7dd2bf6c18eafc76da917a5f4b76c07c9d1f37
sid2261 <siddhartha2251@gmail.com>
2023-09-02 18:56:19 +0530

    nested routes

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,16 +1,46 @@
 import { Routes } from 'react-router-dom';
 import './App.css'
 import { Route } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard/>}></Route>
-      <Route path='/about' element={<About/>}></Route>
-      <Route path='/home' element={<Home/>}></Route>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Route>
     </Routes>
   );
 }
+function Layout(){
+  return (
+    <div className='layout'>
+      <header className='header'>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className='content'>
+        <Outlet/>
+      </main>
+      <footer className='footer'>this is footer</footer>
+    </div>
+  );
+}
+
 function Dashboard(){
   return <h1>This is Dashboard</h1>
 }
